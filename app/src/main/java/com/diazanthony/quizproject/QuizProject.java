e08a1a13c234a5c4759c89d510eff8b6d92fcc69
Anthony Diaz <diazanthony32@gmail.com>
2014-12-19 12:17:47 -0800

    After 2 hrs....

--- app/src/main/java/com/diazanthony/quizproject/QuizProject.java
+++ app/src/main/java/com/diazanthony/quizproject/QuizProject.java
@@ -13,6 +13,11 @@ import android.widget.Toast;
 public class QuizProject extends Activity {
 
     private TextView question;
+    private TextView displayScore;
+
+    private Integer totalQuestions;
+    private Integer score;
+    private Integer questionnum;
 
     private Button trueButton;
     private Button falseButton;
@@ -23,19 +28,38 @@ public class QuizProject extends Activity {
         setContentView(R.layout.activity_quiz_project);
 
         question = (TextView) findViewById(R.id.question);
+        displayScore = (TextView) findViewById(R.id.displayScore);
 
         trueButton = (Button) findViewById(R.id.button);
         falseButton = (Button) findViewById(R.id.button2);
 
-        question.setText(Questions.get().getQuestion());
+        question.setText("Do you Love Us Moises?");
+        questionnum = 1;
+        score = 0;
+        totalQuestions = 1;
 
         trueButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                Toast toast = Toast.makeText(getApplication(),"You Chose True...", Toast.LENGTH_SHORT);
-                toast.show();
+                if (questionnum == 1) {
+
+                    Toast toast = Toast.makeText(getApplication(), "Incorrect...", Toast.LENGTH_SHORT);
+                    toast.show();
+
+                    questionnum = questionnum + 1;
+                    score = score + 0;
+
+                    question.setText("Game Over!");
+
+                    if(questionnum == 2){
+
+                        displayScore.setText(Integer.toString(score));
+
+                    }
+
+                }
 
             }
 
@@ -46,13 +70,27 @@ public class QuizProject extends Activity {
             @Override
             public void onClick(View v) {
 
-                Toast toast = Toast.makeText(getApplication(),"You Chose False...", Toast.LENGTH_SHORT);
-                toast.show();
+                if (questionnum == 1) {
+
+                    Toast toast = Toast.makeText(getApplication(), "Correct!!!", Toast.LENGTH_SHORT);
+                    toast.show();
+
+                    questionnum = questionnum + 1;
+                    score = score + 1;
+
+                    question.setText("Game Over!");
+
+                    if(questionnum == 2){
+
+                        displayScore.setText(Integer.toString(score));
+
+                    }
+
+                }
 
             }
 
         });
-
 
     }
 
